4467a5c47fd2e9ccc4209337d4cd01a3f7316465
Kamil <ninja59234@gmail.com>
2025-03-27 01:04:49 +0100

    Basic multiplayer
    Kaplica

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Values")]
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+ 
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+ 
+    [HideInInspector]
+    public bool canMove = true;
+ 
+    [SerializeField]
+    private float cameraYOffset = 0.4f;
+    private Camera playerCamera;
+ 
+    private Alteruna.Avatar _avatar;
+ 
+    void Start()
+    {
+        _avatar = GetComponent<Alteruna.Avatar>();
+ 
+        if (!_avatar.IsMe)
+            return;
+        
+        characterController = GetComponent<CharacterController>();
+        playerCamera = Camera.main;
+        playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y + cameraYOffset, transform.position.z);
+        playerCamera.transform.SetParent(transform);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+ 
+    void Update()
+    {
+        if (!_avatar.IsMe)
+            return;
+        
+        bool isRunning = false;
+ 
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+ 
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+ 
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+ 
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+ 
+        characterController.Move(moveDirection * Time.deltaTime);
+ 
+        if (canMove && playerCamera != null)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
