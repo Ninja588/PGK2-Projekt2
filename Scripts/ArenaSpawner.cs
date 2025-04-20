c0fbd627cdf9108614db86683758cf00d7f9dc4e
Kamil <ninja59234@gmail.com>
2025-04-20 19:13:43 +0200

    Spawner naprawiony + podstawa do punktow

--- Scripts/ArenaSpawner.cs
+++ Scripts/ArenaSpawner.cs
@@ -4,41 +4,24 @@ using Alteruna;
 public class ArenaSpawner : MonoBehaviour
 {
     private Spawner _spawner;
-    private Transform _arena;
+    [SerializeField] private Transform _arena;
     private bool arenaSpawned = false;
 
     void Awake()
     {
         _spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
-        _arena = GameObject.Find("Arena").transform;
+        _spawner.ForceSync = true;
     }
 
-    void OnEnable()
-    {
-        Multiplayer.Instance.OnOtherUserJoined.AddListener(SpawnArena);
-        //Debug.Log("jest");
-    }
-
-    void OnDisable()
-    {
-        if (Multiplayer.Instance != null) {
-            Multiplayer.Instance.OnOtherUserJoined.RemoveListener(SpawnArena);
-            //Debug.Log("nie ma");
-        }
-    }
-
-    private void SpawnArena(Multiplayer mp, User me)
-    {
-        //Debug.Log("jestem tu");
-        //Debug.Log("ArenaSpawned: " + arenaSpawned + " playerCount: " + mp.GetUsers().Count + " Host: " + me.IsHost);
-        if(!arenaSpawned && mp.GetUsers().Count == 2 && me.IsHost) {
-            //Debug.Log("Respie!");
+    void Update() {
+        if (Multiplayer.Instance.GetUsers().Count == 1 && Multiplayer.Instance.GetUser().IsHost) {
             _spawner.Spawn(0,new Vector3(-16.59253f, 4.500999f, 0f), Quaternion.identity,new Vector3(0.8f, 5f, 9f)).transform.SetParent(_arena); // Paddle Blue
             _spawner.Spawn(1,new Vector3(55.40747f, 4.500999f, 0f), Quaternion.Euler(0, 180f, 0),new Vector3(0.8f, 5f, 9f)).transform.SetParent(_arena); // Paddle Red
             _spawner.Spawn(2, new Vector3(20f, 3.14f, 0f),Quaternion.identity,new Vector3(2f, 2f, 2f)); // Ball(s)
 
             arenaSpawned = true;
         }
+
+        if(arenaSpawned) gameObject.SetActive(false);
     }
-    
 }
